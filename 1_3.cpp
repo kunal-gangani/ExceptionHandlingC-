d6ca905299317b0a16148da5c35975b975681db1
Kunal Gangani <150250846+kunal-gangani@users.noreply.github.com>
2024-02-01 10:07:52 +0530

    Added

--- 1_3.cpp
+++ 1_3.cpp
@@ -1,24 +1,23 @@
 #include<iostream>
 #include<string.h>
 using namespace std;
-
-int main(){
+int main() {
 	int i;
-	char str[50];
-	cout<<"Enter String : ";
-	cin>>str;
-	int size = strlen(str);
-	for(i=1;i<size;i++){
-		try{
-			if(str[i]>65 && str[i]<92){
-				cout<<" Password is Valid.."<<endl;
-			}else{
-				throw 1;
-			}
-		}
-		catch(int x){
-			cout<<"Password is Invalid..";
-		}
-	}
+    char str[50];
+    cout<<"Enter String : ";
+    cin>>str;
+    int size = strlen(str);
+
+    try{
+        for(i=0;i<size;i++) {
+            if(str[i]>='A' && str[i]<='Z'){
+                cout<<"Password is Valid.."<<endl;
+            }else{
+                throw 1;
+            }
+        }
+    }catch (int x){
+        cout<<"Password is Invalid.."<<endl;
+    }
 	return 0;
 }
